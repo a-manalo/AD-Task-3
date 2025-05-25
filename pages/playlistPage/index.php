21225d33271a4f4790369d987ce8f2ded7242fcf
a-manalo <agmnlo.by@gmail.com>
2025-05-25 15:09:50 +0800

    refactor: applied fragmented version

---
+++ pages/playlistPage/index.php
@@ -0,0 +1,14 @@
+<?php
+require BASE_PATH . '/utils/HtmlEscape.utils.php';
+
+require_once BASE_PATH . '/components/templates/head.component.php';
+require_once BASE_PATH . '/components/templates/nav.component.php';
+require_once BASE_PATH . '/components/templates/foot.component.php';
+?>
+
+<?php
+head('Music Playlist');
+navHeader();
+?>
+
+<?php footer(); ?>
