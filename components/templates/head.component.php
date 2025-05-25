e211f3471edf42697525bc959ce843e2b8cdbaad
a-manalo <agmnlo.by@gmail.com>
2025-05-25 15:06:42 +0800

    feat: added poppins font and corrected reference link for Playlist nav

--- components/templates/head.component.php
+++ components/templates/head.component.php
@@ -8,7 +8,7 @@ function head($pageTitle)
         <head>
             <meta charset="utf-8">
             <title><?= htmlEscape($pageTitle ?? 'Music Playlist') ?></title>
-            
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
             <!-- Libraries -->
             <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         </head>
--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -5,12 +5,12 @@ function navHeader()
     <header>
         <nav class="bg-pink-200 backdrop-blur-sm w-full shadow-lg">
             <div class="flex justify-center items-center py-5">
-                <ul class="flex space-x-8">
+                <ul class="flex space-x-8" style="font-family: 'Poppins', sans-serif;">
                     <li>
                         <a href="../index.php" class="text-white font-bold hover:text-black transition-colors duration-200 [text-shadow:_2px_2px_2px_rgb(0_0_0_/_40%)]">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="text-white font-bold hover:text-black transition-colors duration-200 [text-shadow:_2px_2px_2px_rgb(0_0_0_/_40%)]">Playlist</a>
+                        <a href="../pages/playlistPage/index.php" class="text-white font-bold hover:text-black transition-colors duration-200 [text-shadow:_2px_2px_2px_rgb(0_0_0_/_40%)]">Playlist</a>
                     </li>
                 </ul>
             </div>
